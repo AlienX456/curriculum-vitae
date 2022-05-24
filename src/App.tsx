6fcfd887f8323410d44510dd229a7955b7cbe536
alienx456 <estebanelias27dev@gmail.com>
2022-05-23 21:25:52 -0500

    using iconify to get icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import './App.css';
 import { default as text } from './assets/text/text.json'
+import { default as icons } from './assets/text/icons.json'
 import profilePhoto from './assets/image/profilePhoto.jpeg'
 import catComputer from './assets/image/catComputer.gif'
 import dogComputer from './assets/image/dogComputer.gif'
 import ImageTextComposition from './layout/ImageTextComposition/ImageTextComposition';
+import { IconsRow } from './layout/IconsRow/IconsRow';
+
+
+
 
 function App() {
+
   return (
     <div>
       <ImageTextComposition imageSRC={profilePhoto} title={text.profileTitle} text={text.profileText}/>
       <ImageTextComposition imageSRC={catComputer} title={text.workTitle} text={text.workText}/>
+      <IconsRow iconsArray={icons}/>
       <ImageTextComposition imageSRC={dogComputer} title={text.otherSkillsTitle} text={text.otherSkillsText}/>
     </div>
   );
---
+++ src/layout/IconsRow/IconsRow.tsx
@@ -0,0 +1,19 @@
+import classes from './IconsRow.module.css'
+import { Icon } from '@iconify/react';
+
+export function IconsRow(props:any) {
+
+    const iconsDivs = props.iconsArray.map((icon :any) => 
+        <div className={classes.iconColumn}>
+            <Icon icon={icon.iconName} className={classes.icon}/>
+            <p>{icon.iconTitle}</p>
+        </div>
+    );
+
+
+    return (
+        <div className={classes.container}>
+            {iconsDivs}
+        </div>
+    )
+}
